1660bb72dbd9eb027f272d6e0d24b9e4afd33cb1
Iain Tooley <iaintooley1@gmail.com>
2019-10-22 20:40:13 +0100

    added a list and a dropdown that selects a currency

--- src/app.js
+++ src/app.js
@@ -4,7 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
     data: {
-      
-    }
+      currencies: [],
+      selectedCurrency: null
+    },
+    computed: {
+      newTotal: function() {
+        
+      }
+    },
+    mounted(){
+      this.getCurrencies()
+    },
+    methods: {
+      getCurrencies: function() {
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(currencies => this.currencies = currencies)
+      }
+  }
   })
 })
